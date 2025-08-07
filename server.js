385a97f7f75b70a288c613e8bff0164a073fbea7
jceijfizeo <quentin-externe.delaporte@edf.fr>
2025-08-07 16:43:21 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,111 @@
+require('dotenv').config();
+const Prometheus = require('prom-client')
+const express = require('express');
+const http = require('http');
+const { Pool } = require('pg');
+const cors = require('cors');
+Prometheus.collectDefaultMetrics();
+
+const requestHistogram = new Prometheus.Histogram({
+    name: 'http_request_duration_seconds',
+    help: 'Duration of HTTP requests in seconds',
+    labelNames: ['code', 'handler', 'method'],
+    buckets: [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5]
+})
+
+const requestTimer = (req, res, next) => {
+  const path = new URL(req.url, `http://${req.hostname}`).pathname
+  const stop = requestHistogram.startTimer({
+    method: req.method,
+    handler: path
+  })
+  res.on('finish', () => {
+    stop({
+      code: res.statusCode
+    })
+  })
+  next()
+}
+
+const pool = new Pool({
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+});
+const app = express();
+const server = http.createServer(app)
+
+// See: http://expressjs.com/en/4x/api.html#app.settings.table
+const PRODUCTION = app.get('env') === 'production';
+
+// Administrative routes are not timed or logged, but for non-admin routes, pino
+// overhead is included in timing.
+app.get('/ready', (req, res) => res.status(200).json({status:"ok"}));
+app.get('/live', (req, res) => res.status(200).json({status:"ok"}));
+app.get('/metrics', async (req, res, next) => {
+  const metrics = await Prometheus.register.metrics();
+  res.set('Content-Type', Prometheus.register.contentType)
+  res.end(metrics);
+})
+
+// Enable CORS for all routes
+app.use(cors());
+
+// Time routes after here.
+app.use(requestTimer);
+
+// Log routes after here.
+const pino = require('pino')({
+  level: PRODUCTION ? 'info' : 'debug',
+});
+app.use(require('pino-http')({logger: pino}));
+
+app.get('/', (req, res) => {
+  // Use req.log (a `pino` instance) to log JSON:
+  req.log.info({message: 'Hello from Node.js Starter Application!'});
+  res.send('Hello from Node.js Starter Application!');
+});
+
+app.get('/g', async (req, res) => {
+  res.status(200).send(process.env.VALUE);
+})
+
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT NOW()');
+    res.status(200).json(result.rows[0]);
+    client.release();
+  } catch (err) {
+    req.log.error(err, 'Error connecting to db<br>' + 
+      "host: " + process.env.PGHOST + "<br>" +
+      "user: " + process.env.PGUSER + "<br>" +
+      "password: " + process.env.PGPASSWORD + "<br>" +
+      "database: " + process.env.PGDATABASE + "<br>" +
+      "port: " + process.env.PGPORT + "<br>");
+    res.status(500).send('Error connecting to database<br>' + 
+      "host: " + process.env.PGHOST + "<br>" +
+      "user: " + process.env.PGUSER + "<br>" +
+      "password: " + process.env.PGPASSWORD + "<br>" +
+      "database: " + process.env.PGDATABASE + "<br>" +
+      "port: " + process.env.PGPORT + "<br>" 
+    );
+  }
+});
+
+
+app.get('*', (req, res) => {
+  res.status(404).send("Not Found");
+});
+
+app.get('test', (req, res) => {
+  res.status(404).send("COUCOU");
+});
+
+// Listen and serve.
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`App started on PORT ${PORT}`);
+});
